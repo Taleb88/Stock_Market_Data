46e1a63924e9a2e1026c743cdc327bf1da563aa2
Taleb Damaree <tdamaree18@gmail.com>
2024-01-29 13:03:15 -0500

    updates

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import pandas as pd
 
+fundamentals_df = pd.read_excel('master.xlsx',
+                                sheet_name="fundamentals")
+prices_df = pd.read_excel('master.xlsx',
+                          sheet_name="prices")
+prices_split_adjusted_df = pd.read_excel('master.xlsx',
+                                         sheet_name="prices-split-adjusted")
+securities_df = pd.read_excel('master.xlsx',
+                              sheet_name="securities")
+
+print(fundamentals_df.head())

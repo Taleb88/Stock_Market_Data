471045bbb96262deb29fe532c2f7f53f5ee4478a
Taleb Damaree <tdamaree18@gmail.com>
2024-01-29 23:02:28 -0500

    updates

--- main.py
+++ main.py
@@ -14,6 +14,15 @@ securities_df = pd.read_excel('master.xlsx',
 
 fundamentals_condensed_df = pd.DataFrame()
 id = fundamentals_df.iloc[:,0]
-fundamentals_condensed_df['id'] = id.copy()
+fundamentals_condensed_df['ID'] = id.copy()
+ticker_symbol = fundamentals_df.iloc[:,1]
+fundamentals_condensed_df['Ticker Symbol'] = ticker_symbol.copy()
+period_ending = fundamentals_df.iloc[:,2]
+fundamentals_condensed_df['Period Ending'] = period_ending.copy()
+accounts_payable = fundamentals_df.iloc[:,3]
+fundamentals_condensed_df['Accounts Payable'] = accounts_payable.copy()
+accounts_receivable = fundamentals_df.iloc[:,4]
+fundamentals_condensed_df['Accounts Receivable'] = accounts_receivable.copy()
+
 
 fundamentals_condensed_df.to_excel('fundamentals_condensed_df.xlsx', index=False)

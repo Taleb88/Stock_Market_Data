2064994fd08eb3b456488248b3399a600442cde9
Taleb Damaree <tdamaree18@gmail.com>
2024-02-05 23:27:48 -0500

    updates

--- main.py
+++ main.py
@@ -304,6 +304,14 @@ plt.ylabel("Earnings Per Share")
 plt.pie(file['Earnings Per Share'],labels=file['For Year'])
 plt.show()
 
+# BMY yearly earnings per share pie chart
+file = pd.read_excel('bmy_stock_yearly_earnings_per_share_df.xlsx')
+plt.title('BMY Yearly Earnings Per Share')
+plt.xlabel("For Year")
+plt.ylabel("Earnings Per Share")
+plt.pie(file['Earnings Per Share'],labels=file['For Year'])
+plt.show()
+
 
 # *AUTOMATICALLY ADJUSTING WIDTH FOR ALL COLUMNS*
 

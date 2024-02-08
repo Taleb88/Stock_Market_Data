23133024e2508c5d83f07ff84a361d8834659244
Taleb Damaree <tdamaree18@gmail.com>
2024-02-07 22:57:51 -0500

    updates

--- main.py
+++ main.py
@@ -489,6 +489,18 @@ yearly_earnings_per_share_pivot_table = \
 yearly_earnings_per_share_pivot_table.\
     to_excel('yearly_earnings_per_share_pivot_table.xlsx')
 
+#lowest value -> negative yearly earnings per year
+#2012
+def highlight_min(x):
+   min_val = x.min()
+   return ['background-color: yellow' if val == min_val else '' for val in x]
+
+negative_yearly_earnings_per_share_2012_df = negative_yearly_earnings_per_share_2012_df.style.apply(highlight_min)
+
+negative_yearly_earnings_per_share_2012_df.\
+    to_excel('negative_yearly_earnings_per_share_2012_df.xlsx',
+             index=False)
+
 
 # *CHARTS*
 # using matplotlib
